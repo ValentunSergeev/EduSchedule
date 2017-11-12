51fd0502020f206d380d8ea198c81f006fe6a803
Valentun Sergeev <karbon4ic@gmail.com>
2017-11-12 15:14:27 +0600

    remove outdated code

--- app/src/main/java/com/valentun/eduschedule/data/persistance/PreferenceManager.java
+++ app/src/main/java/com/valentun/eduschedule/data/persistance/PreferenceManager.java
@@ -4,9 +4,6 @@ package com.valentun.eduschedule.data.persistance;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class PreferenceManager {
@@ -21,23 +18,6 @@ public class PreferenceManager {
     public PreferenceManager(Context context) {
         this.context = context;
         preferences = getDefaultSharedPreferences(context);
-    }
-
-    // TODO remove
-    public String getTestData() {
-        String json;
-        try {
-            InputStream is = context.getAssets().open("test_data.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
     }
 
     public boolean isGroupChosen() {
